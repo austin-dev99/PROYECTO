fac767b117260426e8195c0e5573ad3b74ba6839
DEV-ALEXISRJ <alexmecaronix55@hotmail.com>
2025-08-28 17:03:53 -0600

    cambios en buscador controller

--- backend/buscador/src/main/java/com/buscador/controller/BuscadorController.java
+++ backend/buscador/src/main/java/com/buscador/controller/BuscadorController.java
@@ -14,7 +14,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/buscador")
 public class BuscadorController {
 
     private final RestTemplate elasticRest;
